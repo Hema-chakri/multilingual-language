7c18d40c669795c1d6818a39dce10a27ee3a3025
Hema-chakri <hemavathi.gadi@gmail.com>
2024-02-20 08:54:30 +0000

    all test cases passed

--- src/components/MultilingualGreetings/index.js
+++ src/components/MultilingualGreetings/index.js
@@ -1,31 +1,60 @@
+import './index.css'
 import {Component} from 'react'
-import GreetingLanguage from '../GreetingLanguage'
 
-const MultilingualGreetings = props => {
-  const {languageGreetingsList} = props
-
-  const onClickSelectedLanguage = button => {
-    ;<img
-      src={languageGreetingsList[0].imageUrl}
-      alt={languageGreetingsList[0].imageAltText}
-    />
+class MultilingualGreetings extends Component {
+  state = {
+    resultImage:
+      'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/english-greetings-img.png',
+    altText: 'english',
   }
 
-  return (
-    <>
-      <h1>Multilingual Greetings</h1>
-      <ul>
-        {languageGreetingsList.map(eachItem => (
-          <button
-            type="button"
-            onClick={onClickSelectedLanguage(eachItem.buttonText)}
-          >
-            {eachItem.buttonText}
-          </button>
-        ))}
-      </ul>
-    </>
-  )
+  displaySelectedGreet = greet => {
+    if (greet.buttonText === 'English') {
+      this.setState({
+        resultImage:
+          'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/english-greetings-img.png',
+        altText: 'english',
+      })
+    } else if (greet.buttonText === 'Tamil') {
+      this.setState({
+        resultImage:
+          'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/tamil-greetings-img.png',
+        altText: 'tamil',
+      })
+    } else if (greet.buttonText === 'Telugu') {
+      this.setState({
+        resultImage:
+          'https://assets.ccbp.in/frontend/react-js/multilingual_greeting/telugu-greetings-img.png',
+        altText: 'telugu',
+      })
+    }
+  }
+
+  render() {
+    const {languageGreetingsList} = this.props
+    const {resultImage, altText} = this.state
+
+    return (
+      <div className="app-container">
+        <h1 className="heading">Multilingual Greetings</h1>
+
+        <ul className="list-container">
+          {languageGreetingsList.map(eachGreet => (
+            <li key={eachGreet.id} className="list-item">
+              <button
+                type="button"
+                onClick={() => this.displaySelectedGreet(eachGreet)}
+                className="button-style"
+              >
+                <p>{eachGreet.buttonText}</p>
+              </button>
+            </li>
+          ))}
+        </ul>
+        <img src={resultImage} alt={altText} className="greet-image" />
+      </div>
+    )
+  }
 }
 
 export default MultilingualGreetings
